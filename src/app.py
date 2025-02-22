25df5fa1ad0e56d56126fc343c06bd0a47a07d46
chanathipjjj444 <chanathip2320@gmail.com>
2025-02-22 21:19:45 +0700

    Add prime check

--- src/app.py
+++ src/app.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 def getcode():
     return jsonify({"Hello W0rld": "aaaa4"})
 
-@app.route('/isprime/<a:int>', methods=["GET"])
-def getprime(a):
-    for i in range (2,(int(a / 2) + 1)):
-        if a % i == 0:
-            return jsonify({"Result": "Not Prime"})
-    
-    return jsonify({"Result": "Is Prime"})
+@app.route('/is_prime/<a>', methods=["GET"])
+def is_prime(a):
+    try:
+        a = int(a)
+        if a <= 1:
+            return "False"
+        
+        for i in range (2,(int(a / 2) + 1)):
+            if a % i == 0:
+                return "False"
+        
+        return "True"
+    except ValueError:
+        return "Error"
             
 
 @app.route("/plus/<a>/<b>", methods=["GET"])
--- src/test_flask.py
+++ src/test_flask.py
@@ -9,6 +9,21 @@ class TestAPI(unittest.TestCase):
         response = self.client.get("/plus/4/3")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['Result is'], 7)
+    
+    def test_when_x_is_17(self):
+        response = self.client.get("/is_prime/17")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "True")
+    
+    def test_when_x_is_36(self):
+        response = self.client.get("/is_prime/36")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "False")
+        
+    def test_when_x_is_13219(self):
+        response = self.client.get("is_prime/13219")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "True")
 
 if __name__ == "__main__":
     unittest.main()
